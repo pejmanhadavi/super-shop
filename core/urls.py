e10dd3ff9261c22dcbf6b654ce84579b6f707179
phc613 <pejmanhadavi77@gmail.com>
2020-02-16 11:17:04 +0330

    Add initial base template and partials and use them.

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', TemplateView.as_view(template_name='pages/index.html'), name='index'),
 ]
