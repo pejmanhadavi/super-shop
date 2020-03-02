6aece68128c922b4ab062c3bc6ad3b01ed2af95d
passive989 <passive76989@gmail.com>
2020-03-03 01:00:10 +0330

    adding category to project

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from .views import ShopListView,ProductDetailView
+from .views import ShopListView,ProductDetailView,CategoryList
 
 
 urlpatterns = [
     path('', ShopListView.as_view(), name='index'),
     path('<slug:slug>', ProductDetailView.as_view(), name='product_detail'),
+    path('category/<slug:slug>', CategoryList.as_view(), name='index'),
 ]
--- product/views.py
+++ product/views.py
@@ -10,7 +10,35 @@ class ShopListView(ListView):
     template_name = 'pages/index.html'
     context_object_name = 'products'
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ShopListView, self).get_context_data(**kwargs)
+        # Add in the publisher
+        context['categories'] = Category.objects.order_by('name')  
+        return context  
+
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'pages/product_detail.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
+        # Add in the publisher
+        context['categories'] = Category.objects.order_by('name')  
+        return context  
+
+
+class CategoryList(ListView):
+    model = Category
+    template_name = 'pages/index.html'
+    context_object_name = 'categories'
+    queryset = Category.objects.order_by('name')  
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(CategoryList, self).get_context_data(**kwargs)
+        # Add in the publisher
+        context['categories'] = Category.objects.order_by('name')  
+        return context  
