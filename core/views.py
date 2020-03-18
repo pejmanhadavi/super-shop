74a8515450ae76151be55a166febfcb0baca7a9a
passive989 <passive76989@gmail.com>
2020-03-18 21:49:20 +0330

    add category list to contact page

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import Contact
 from django.contrib import messages
+from product.models import Category, Product
+from django.views.generic import (ListView, DetailView, View) 
 
 
 def contact(request):
@@ -11,7 +13,34 @@ def contact(request):
           contact = Contact.objects.create(name=name, email=email, message=message)
           contact.save()
           messages.success(request, 'OK thanks ill call you back soon :)) ')
-          return redirect('contact')
+          context = {
+                'categories' : Category.objects.order_by('name')  
+            }          
+          return render(request, 'pages/contact.html', context)
         else:
-          return render(request, 'pages/contact.html')
+          context = {
+                'categories' : Category.objects.order_by('name')  
+            }               
+          return render(request, 'pages/contact.html', context)
      
+
+class CategoryList(ListView):
+    model = Product
+    template_name = 'pages/index.html'
+    context_object_name = 'products'
+    queryset = Category.objects.order_by('name') 
+
+     
+    def get_context_data(self, **kwargs):
+        context = super(CategoryList, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.order_by('name')  
+        return context  
+
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = Product.objects.all()
+        slug = self.kwargs['slug']
+        if slug:
+            category = Category.objects.filter(slug=slug).first()
+            queryset = queryset.filter(category=category.id)
+        return queryset         
